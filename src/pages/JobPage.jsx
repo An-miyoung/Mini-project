5ebcde2b4692013ec59c50443d3cd67ef6205b59
miyoung <noonchicat@gmail.com>
2022-02-03 11:59:21 +0900

    ask, job 페이지 변경

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,50 @@
-import PostList from "../components/PostList";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import useGetPost, { getStoriesPage } from "../hooks/useGetPost";
+import Header from "../components/Header";
+import BottomNavi from "../components/BottomNavi";
+import Post from "../components/Post";
+import Comments from "../components/Comments";
 
 export default function JobPage() {
   const type = "job";
+  // console.log(type);
+
+  const posts = useGetPost(type ? type : "top");
+
+  // console.log(posts);
+  useEffect(() => {
+    const divPage = document.getElementById("div-page");
+    const divPageScroll = function (event) {
+      if (
+        event.target.scrollHeight <=
+        event.target.clientHeight + event.target.scrollTop
+      ) {
+        getStoriesPage(type);
+      }
+    };
+    divPage.addEventListener("scroll", divPageScroll);
+    return function () {
+      divPage.removeEventListener("scroll", divPageScroll);
+    };
+  }, [type]);
+
   return (
-    <div>
-      <PostList type={type} />
+    <div className="container">
+      <Header name={type} />
+
+      <div className="page" id="div-page">
+        <div className="seach-bar">SEARCH-BAR</div>
+        {/* height 를 직접주고 Card 를 바로 부르도록 수정할것 */}
+        {posts.map(
+          ({ data: post }) =>
+            post && <Post key={post.id} post={post} type={type} />
+        )}
+      </div>
+      <BottomNavi />
+      <Routes>
+        <Route path="comments/:id" element={<Comments />} />
+      </Routes>
     </div>
   );
 }
